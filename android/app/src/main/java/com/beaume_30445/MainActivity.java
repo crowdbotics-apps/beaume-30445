a6f8e6245faec6afb4cd319a36ece6887bda60af
Crowdbotics <team@crowdbotics.com>
2021-09-10 18:37:11 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/beaume_30445/MainActivity.java
@@ -0,0 +1,15 @@
+package com.beaume_30445;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "beaume_30445";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "beaume_30445Identifier",
+  url: "https://beaume_30445Identifier.botics.co",
+  api: "https://beaume_30445Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
